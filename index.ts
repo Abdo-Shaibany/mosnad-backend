7aa142d59f37f0b8178396e0ec2c03de49b19c6f
Abdo-Shaibany <abdo.shaibany@gmail.com>
2024-08-27 13:15:18 +0300

    backend is ready

--- index.ts
+++ index.ts
@@ -1,25 +1,15 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import suppliers from './src/routes/suppliers';
-import banks from './src/routes/banks';
 import banksLocation from './src/routes/user';
-import bankCurrencies from './src/routes/banks_currencies';
-import products from './src/routes/products';
-import images from './src/routes/images';
-import auth from './src/routes/auth';
-import accounts from './src/routes/account';
-import seller from './src/routes/seller';
-import inventorySupply from './src/routes/supply_inventory';
-import productForSales from './src/routes/product_for_sales';
-import transactions from './src/routes/transaction';
-import categories from './src/routes/categories';
-import inventories from './src/routes/inventories';
 import logger from './src/middleware/logger';
 import notFound from './src/middleware/not-found';
-import errorHandler from './src/middleware/error';
 import cors from 'cors';
-import { authorizeRoles } from 'src/middleware/role';
+
+import activty from "./src/routes/activity";
+import auth from './src/routes/auth';
+import roles from './src/routes/roles';
+import user from './src/routes/user';
 
 const port = process.env['PORT'] || 5000;
 
@@ -41,25 +31,13 @@ app.use(logger);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/suppliers', suppliers);
-
-app.use('/api/products', products);
-app.use('/api/inventory-supplies', inventorySupply);
-app.use('/api/products-for-sales', productForSales);
-app.use('/api/transactions', transactions);
-
-app.use('/api/categories', categories);
-app.use('/api/inventories', inventories);
-app.use('/api/banks', banks);
-app.use('/api/banks-locations', banksLocation);
-app.use('/api/banks-currencies', bankCurrencies);
-app.use('/api/images', images);
+app.use('/api/activty', activty);
 app.use('/api/auth', auth);
-app.use('/api/accounts', accounts);
-app.use('/api/seller', seller);
+app.use('/api/roles', roles);
+app.use('/api/user', user);
+
 
 app.use(notFound);
-app.use(errorHandler);
 
 // Set up a route for file uploads
 
